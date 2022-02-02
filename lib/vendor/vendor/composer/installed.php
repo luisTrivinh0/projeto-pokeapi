d38897bfd1d9884cb1f6afee9901c5d327c935d8
Luis Trivinho <luis.trivinho@icloud.com>
2022-02-02 19:09:51 -0300

    confirmar

---
+++ lib/vendor/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'danrovito/pokephp' => array(
+            'pretty_version' => '1.3',
+            'version' => '1.3.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../danrovito/pokephp',
+            'aliases' => array(),
+            'reference' => 'd2a1d5a97f241a249a5f101d80042f3a4bf9d430',
+            'dev_requirement' => false,
+        ),
+    ),
+);
